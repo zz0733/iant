1309ee3518147cb0d19dd8f0e9aa8c7ac1c940fb
lezo <lcstore@126.com>
2017-04-14 01:46:43 +0800

    add es

---
+++ lua/api/access_link.lua
@@ -0,0 +1,6 @@
+local args = ngx.req.get_uri_args()
+-- if not args.a or not args.b or not param.is_number(args.a, args.b) then
+--     ngx.exit(ngx.HTTP_BAD_REQUEST)
+--     return
+-- end
+-- ngx.say("access " .. ngx.req.get_method())
