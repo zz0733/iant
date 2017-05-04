ad1a9d4b869b1427ccc1b27e618ca5f7557885f5
lezo <lcstore@126.com>
2017-05-04 21:28:21 +0800

    add retry

--- lua/api/task.lua
+++ lua/api/task.lua
@@ -37,11 +37,45 @@ if 'insert' == method  then
     end
     local body = cjson_safe.encode(message)
     ngx.say(body)
+elseif 'retry' == method  then
+  local total_count = 0
+  local retry_count = 0
+  local err_count = 0
+  for _,v in ipairs(body_json) do
+      local task = v.task
+      local status = v.status
+      total_count = total_count + 1
+      if status == 0 and task and task.params then
+          local params_obj = cjson_safe.decode(task.params)
+          if params_obj and params_obj.retry then
+            local retry_obj = params_obj.retry
+            local index = retry_obj.index or 0
+            local total = retry_obj.total or 3
+            index = index + 1
+            if index <= total then
+              params_obj.retry = { index = index, total = total }
+              task.params = cjson_safe.encode(params_obj)
+              retry_count = retry_count + 1
+              local task_val = cjson_safe.encode(task)
+              local len, err = shared_dict:lpush(task_queue_key, task_val)
+              if err then
+                  err_count = err_count + 1
+                  log(CRIT,"shared_dict:lpush:" .. task_val .. ",cause:", err)
+              end
+            end
+          end
+          
+      end
+  end
+  local message = {}
+  message.data = {err_count = err_count, total_count = total_count, retry_count = retry_count}
+  message.code = 200
+  local body = cjson_safe.encode(message)
+  ngx.say(body)
 elseif 'nexts' == method then
     function contains( table, value )
       if table then
         for _,v in ipairs(table) do
-            log(ERR,"handlers:" .. v)
             if value == v then
               return true
             end
--- lua/app/snap_data.lua
+++ lua/app/snap_data.lua
@@ -1,7 +1,8 @@
 local util_request = require "util.request"
 local data = util_request.post_body(ngx.req)
-local resp, _ = ngx.location.capture_multi{
+local resp, _, _ = ngx.location.capture_multi{
       {"/api/collect.json", { args = { method = "insert" }, body = data }},
-      {"/api/task.json", { args = { method = "nexts" }, body = data }}
+      {"/api/task.json", { args = { method = "nexts" }, body = data }},
+      {"/api/task.json", { args = { method = "retry" }, body = data }}
 }
 ngx.say(resp.body)
