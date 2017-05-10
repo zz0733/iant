c2d37e64106c8f0e8274fe6f714a2fd98a51ddae
lezo <lcstore@126.com>
2017-05-10 22:55:16 +0800

    u match

--- lua/timer/boot_timer.lua
+++ lua/timer/boot_timer.lua
@@ -1,4 +1,4 @@
 local load_task_timer = require("timer.load_task")
 local load_script_timer = require("timer.load_script")
 local handle_collect_timer = require("timer.handle_collect")
-local link_match_content_timer = require("timer.link_match_content")
+-- local link_match_content_timer = require("timer.link_match_content")
