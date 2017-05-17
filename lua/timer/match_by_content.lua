25cb8b3c6258e139823d7abd721fca376329673a
lezo <lcstore@126.com>
2017-05-17 22:23:47 +0800

    u log

--- lua/timer/match_by_content.lua
+++ lua/timer/match_by_content.lua
@@ -144,7 +144,7 @@ local update_match_doc = function ( doc, hits )
                          local old_target = target_map[doc._id]
                          local new_target = {id = doc._id, score = score, status=0 }
                          if not util_table.equals(old_target, new_target) then
-                             target_map[target.id] = new_target
+                             target_map[new_target.id] = new_target
                              local dest_targets = {}
                              for k,v in pairs(target_map) do
                                  dest_targets[#dest_targets + 1] = v
