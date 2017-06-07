5d7030cb121c5ceb0e2098c1f53ef2d6f30a56df
lilinchong <lilinchong@baidu.com>
2017-06-07 12:58:38 +0800

    u content

--- lua/dao/content_dao.lua
+++ lua/dao/content_dao.lua
@@ -111,7 +111,6 @@ function _M:query_by_region( from, size, region,fields )
 	local body = {
 	  from = from,
 	  size = size,
-	  sort = {_score = { order = "desc"}, "article.year" = { order = "desc"}}
 	  _source = fields,
 	  query = {
 	    nested = {
