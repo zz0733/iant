b66ba6ee0b9f6748b7142abe281bca9a6ab6a0ab
lezo <lcstore@126.com>
2017-08-27 11:17:06 +0800

    use lua-resty-http

--- lua/local/img_magick.lua
+++ lua/local/img_magick.lua
@@ -10,10 +10,12 @@ local content_dao = require "dao.content_dao"
 local req_method = ngx.req.get_method()
 local args = ngx.req.get_uri_args()
 
-local http = require("socket.http")
+-- local http = require("socket.http")
 local magick = require("magick.gmwand")
 local lfs = require("lfs")
 local ltn12 = require("ltn12")
+
+local http = require("resty.http")
 
 
 local io_open = io.open
@@ -49,28 +51,57 @@ if  not post_body then
 end
 
 
+-- function getImageByURL2( url )
+--     local headers  = { 
+--        ['Referer'] = 'https://movie.douban.com/',
+--        ['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36'
+--     }
+--     local lastCode;
+--     for i=1,3 do
+--         local t = {}
+--         local params = {
+--             url = url,
+--             sink = ltn12.sink.table(t)
+--         }
+--         params.headers = headers
+--         local _, code = http.request(params)
+--         if code == 200 then
+--             return table.concat(t),code
+--         elseif code == 404 then
+--             return nil,code
+--         else
+--             log(ERR,"imgURL:"..tostring(url) .. ",code:" .. tostring(code) ..",retry:" .. tostring(i))
+--         end
+--         lastCode = code
+--     end
+--     return nil,lastCode
+-- end
 function getImageByURL( url )
     local headers  = { 
        ['Referer'] = 'https://movie.douban.com/',
        ['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36'
     }
+    log(ERR,"xxxxxximgURL:" .. tostring(url))
     local lastCode;
     for i=1,3 do
-        local t = {}
-        local params = {
-            url = url,
-            sink = ltn12.sink.table(t)
-        }
-        params.headers = headers
-        local _, code = http.request(params)
-        if code == 200 then
-            return table.concat(t),code
-        elseif code == 404 then
-            return nil,code
+        local httpc = http.new()
+        local resp, err = httpc:request_uri(url, {
+            method = "GET",
+            headers = headers
+          })
+        if not resp then
+            log(ERR,"imgURL:"..tostring(url) .. ",cause:" .. tostring(err) ..",retry:" .. tostring(i))
         else
-            log(ERR,"imgURL:"..tostring(url) .. ",code:" .. tostring(code) ..",retry:" .. tostring(i))
+            if resp.status == 200 then
+                return resp.body,resp.status
+            elseif resp.status == 404 then
+                return nil,resp.status
+            else
+                log(ERR,"imgURL:"..tostring(url) .. ",code:" .. tostring(resp.status) ..",retry:" .. tostring(i))
+            end
+            lastCode = resp.status
         end
-        lastCode = code
+
     end
     return nil,lastCode
 end
@@ -102,7 +133,7 @@ function handleData(hits)
                     str_img = ngx.re.sub(str_img, "[%.]webp", ".jpg")
                     local strBody, code = getImageByURL(str_img)
                     if code == 404 then
-                        log(ERR,"id:"..tostring(v._id)..",imgURL:"..tostring(url) .. ",code:" .. tostring(code) )
+                        log(ERR,"id:"..tostring(v._id)..",imgURL:"..tostring(str_img) .. ",code:" .. tostring(code) )
                     end
                     if strBody and string.len(strBody) > 0 then
                         local md5 = resty_md5:new()
@@ -119,6 +150,7 @@ function handleData(hits)
                         dv.content =  'http://www.lezomao.com/img/' .. name
 
                         local img = magick.load_image_from_blob(strBody)
+                        log(ERR,"load_image:"..tostring(img)..",bodyLen:"..tostring(string.len(strBody)))
                         if img then
                             -- log(ERR,"width:" .. img:get_width() .. ",height:" .. img:get_height());
                             for _,sv in ipairs(size_arr) do
@@ -199,7 +231,7 @@ local scrollId = nil
 local index = 0
 local total = nil
 local begin = ngx.now()
-while true do
+while false do
      index = index + 1;
      local data,err;
      local start = ngx.now()
@@ -237,5 +269,15 @@ if not scrollId then
     params.scroll_id = scrollId
     sourceClient:clearScroll(params)
 end
+local hits = {}
+local digests = {}
+local digest = { sort = "img", content = "https://img1.doubanio.com/view/movie_poster_cover/lpst/public/p2424225097.webp"}
+-- local digest = { sort = "img", content = "https://holmesian.org/usr/themes/Holmesian/image/avatar.jpg"}
+table_insert(digests,digest)
+local hit = { _id = 1, _source = {
+    digests = digests
+}}
+table_insert(hits,hit)
+handleData(hits)
 local body = cjson_safe.encode(message)
 ngx.say(body)
