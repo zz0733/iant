904c89b8879c57df8029f31449628315ed5f712c
lilinchong <lilinchong@baidu.com>
2017-10-27 23:11:05 +0800

    u tour

--- webapp/assets/js/movie/torrent.js
+++ webapp/assets/js/movie/torrent.js
@@ -1,11 +1,13 @@
 var $document = $(document);
 $document.ready(function() {
-		initClipboard()
+     	initClipboard()
 
 		$('.btn-down:contains(百度离线)').on('click',function(){
              var self = this
              var toURL= 'http://pan.baidu.com/disk/home'
-             window.open(toURL)
+             // window.open(toURL)
+                  		 initTour()
+
 		})
 		$('.btn-down:contains(百度云盘)').on('click',function(){
              var self = this
@@ -23,6 +25,7 @@ $document.ready(function() {
              var toURL = urlcodesc.encode(toURL, "thunder");
              window.location.href = toURL
 		})
+
 
 });
 
@@ -62,4 +65,58 @@ function initClipboard(){
 	  e.clearSelection();
 	  showTooltip(e.trigger, '复制失败');
 	});
+}
+
+function initTour(){
+
+var tour = new Tour({
+  name: "tour",
+  steps: [],
+  container: "body",
+  smartPlacement: true,
+  keyboard: true,
+  storage: window.localStorage,
+  debug: false,
+  backdrop: false,
+  backdropContainer: 'body',
+  backdropPadding: 0,
+  redirect: true,
+  orphan: false,
+  duration: false,
+  delay: false,
+  basePath: "",
+  template: '<div class="popover tour-tour tour-tour-1 fade top in" role="tooltip" id="step-1" style="top: 334.85px; left: 502px; display: block;"> <div class="arrow" style="left: 50%;"></div><div class="popover-content">Easy is better, right? The tour is up and running with just a few options and steps.</div> <div class="popover-navigation"> <div class="btn-group"> <button class="btn btn-sm btn-default" data-role="prev">« Prev</button> <button class="btn btn-sm btn-default" data-role="next">Next »</button>  </div> <button class="btn btn-sm btn-default" data-role="end">End tour</button> </div> </div>',
+  afterGetState: function (key, value) {},
+  afterSetState: function (key, value) {},
+  afterRemoveState: function (key, value) {},
+  onStart: function (tour) {},
+  onEnd: function (tour) {},
+  onShow: function (tour) {},
+  onShown: function (tour) {},
+  onHide: function (tour) {},
+  onHidden: function (tour) {},
+  onNext: function (tour) {},
+  onPrev: function (tour) {},
+  onPause: function (tour, duration) {},
+  onResume: function (tour, duration) {},
+  onRedirectError: function (tour) {}
+});
+tour.addStep({
+    element: "#btn-copy",
+    placement: "right",
+    title: "第一步",
+    content: "①复制资源链接"
+  })
+  tour.addStep({
+    element: "#btn-list",
+    placement: "bottom",
+    title: "第二步",
+    content: "②获取资源"
+  })
+    console.log('init.......')
+	// Initialize the tour
+	tour.init();
+console.log('start.......')
+	// Start the tour
+	tour.start(true);
 }
