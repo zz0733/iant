e29c1642afc0930a2742f2c53debd26013beb35d
lezo <lcstore@126.com>
2018-06-02 20:43:12 +0800

    u cms params

--- lua/movie/cms/link.lua
+++ lua/movie/cms/link.lua
@@ -36,8 +36,15 @@ local body = {
 		}
 	}
 local bool = body.query.bool
-if args.status then
-	local must_arr = {} 
+if args.video then
+	local must_arr = bool.must or {} 
+	table.insert(must_arr, {
+	    match = { video = args.video }
+	})
+    bool.must = must_arr
+end
+if args.status  then
+	local must_arr = bool.must or {} 
 	table.insert(must_arr, {
 	    match = { status = args.status }
 	})
