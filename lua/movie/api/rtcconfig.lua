e69c940ea2e7c4646f908046a53daf9fc8016903
lezo <lcstore@126.com>
2018-06-11 22:51:01 +0800

    add rtcconfig

---
+++ lua/movie/api/rtcconfig.lua
@@ -0,0 +1,13 @@
+local cjson_safe = require "cjson.safe"
+
+local log = ngx.log
+local ERR = ngx.ERR
+local CRIT = ngx.CRIT
+
+local servers = {}
+table.insert(servers, {urls = "stun:stun4.l.google.com:19302"})
+table.insert(servers, {urls = "stun:global.stun.twilio.com:3478?transport=udp"})
+local message = {}
+message.code = 200
+message.data = { iceServers = servers }
+ngx.say(cjson_safe.encode(message))
