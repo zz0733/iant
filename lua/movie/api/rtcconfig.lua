fe0572481dd39308cc69bb7264530a540c388e68
lezo <lcstore@126.com>
2018-06-17 22:54:51 +0800

    u rtc2

--- lua/movie/api/rtcconfig.lua
+++ lua/movie/api/rtcconfig.lua
@@ -8,7 +8,8 @@ local servers = {}
 -- table.insert(servers, {urls = "stun:stun.1und1.de"})
 -- table.insert(servers, {urls = "stun:stun.voiparound.com"})
 -- table.insert(servers, {urls = "stun:stun.xten.com"})
-table.insert(servers, {urls = "stun:stun.aa.net.uk:3479?transport=udp"})
+-- table.insert(servers, {urls = "stun:stun.aa.net.uk:3479?transport=udp"})
+table.insert(servers, {urls = "stun:stun.sonetel.net"})
 -- table.insert(servers, {urls = "stun:stun4.l.google.com:19302"})
 -- table.insert(servers, {urls = "stun:global.stun.twilio.com:3478?transport=udp"})
 local message = {}
