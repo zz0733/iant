4188a34ae16cd7bfff269d6d3f28564f173f2e06
lezo <lcstore@126.com>
2018-06-18 09:53:39 +0800

    ice ssdb

--- lua/movie/api/rtcconfig.lua
+++ lua/movie/api/rtcconfig.lua
@@ -1,18 +1,17 @@
 local cjson_safe = require "cjson.safe"
+local ssdb_ice = require "ssdb.ice"
 
 local log = ngx.log
 local ERR = ngx.ERR
 local CRIT = ngx.CRIT
 
-local servers = {}
--- table.insert(servers, {urls = "stun:stun.1und1.de"})
--- table.insert(servers, {urls = "stun:stun.voiparound.com"})
--- table.insert(servers, {urls = "stun:stun.xten.com"})
--- table.insert(servers, {urls = "stun:stun.aa.net.uk:3479?transport=udp"})
-table.insert(servers, {urls = "stun:stun.sonetel.net"})
--- table.insert(servers, {urls = "stun:stun4.l.google.com:19302"})
--- table.insert(servers, {urls = "stun:global.stun.twilio.com:3478?transport=udp"})
+local iceConfig = ssdb_content:getValue()
+if not iceConfig then
+	local servers = {}
+	table.insert(servers, {urls = "stun:stun.1und1.de"})
+	iceConfig = iceConfig
+end
 local message = {}
 message.code = 200
-message.data = { iceServers = servers }
+message.data = { iceServers = iceConfig }
 ngx.say(cjson_safe.encode(message))
