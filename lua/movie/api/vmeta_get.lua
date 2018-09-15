b7134213b15db3555d79e6026838437c5f658f9f
lezo <lcstore@126.com>
2018-09-15 14:08:23 +0800

    change bad vmeta

--- lua/movie/api/vmeta_get.lua
+++ lua/movie/api/vmeta_get.lua
@@ -3,6 +3,9 @@ local util_request = require "util.request"
 local util_movie = require "util.movie"
 local util_context = require "util.context"
 local ssdb_vmeta = require "ssdb.vmeta"
+local ssdb_meta = require "ssdb.meta"
+
+local bit = require("bit") 
 
 local req_method = ngx.req.get_method()
 local args = ngx.req.get_uri_args()
@@ -35,6 +38,15 @@ if not vmeta then
 end
 if not vmeta.body and vmeta.url then
 	vmeta.body = "#EXTM3U\n#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=800000,RESOLUTION=1080x608\n" .. vmeta.url
+end
+if string.match(vmeta.body, "/odflv/api.php") then
+	local hasMeta = ssdb_meta:get(metaId)
+    hasMeta.id = metaId
+    hasMeta.cstatus = bit.bxor(hasMeta.cstatus or 0, 2)
+    hasMeta.pstatus = 0
+    local modifyArr = {}
+	table.insert(modifyArr, hasMeta)
+	meta_dao:save_metas( modifyArr )
 end
 -- log(ERR,"vmeta:" .. cjson_safe.encode(vmeta))
 ngx.say(vmeta.body)
