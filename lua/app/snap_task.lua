dc4c42463ebb0c3db2ae50508057d30d83d90dc5
lezo <lcstore@126.com>
2018-09-24 20:56:33 +0800

    u assign log

--- lua/app/snap_task.lua
+++ lua/app/snap_task.lua
@@ -3,12 +3,19 @@ local util_request = require "util.request"
 local util_table = require "util.table"
 local ssdb_task = require "ssdb.task"
 
+local log = ngx.log
+local ERR = ngx.ERR
+local CRIT = ngx.CRIT
+
 -- local args = ngx.req.get_uri_args()
 -- local resp = ngx.location.capture(
 --                 "/api/task.json", { args={method = "getmore" }}
 -- )
 -- ngx.say(resp.body)
 
+local req_method = ngx.req.get_method()
+local args = ngx.req.get_uri_args()
+local client = args.client
 local max = 5
 local message = {}
 message.code = 200
@@ -17,6 +24,8 @@ if err then
 	message.error = err
     message.code = 500
 else
+	local count = #assignArr
+	log(ERR,"assign:" .. tostring(client) .. ",count:" .. count)
 	message.data = assignArr
 end
 local body = cjson_safe.encode(message)
