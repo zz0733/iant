593449a5c4c0843f4fc3b289ac2bcf5171a19e9d
lezo <lcstore@126.com>
2018-10-28 11:13:14 +0800

    add remove

---
+++ lua/local/meta_all_remove.lua
@@ -0,0 +1,58 @@
+local cjson_safe = require "cjson.safe"
+local util_request = require "util.request"
+local util_table = require "util.table"
+local util_context = require "util.context"
+local meta_dao = require "dao.meta_dao"
+local ssdb_meta = require "ssdb.meta"
+
+local log = ngx.log
+local ERR = ngx.ERR
+local CRIT = ngx.CRIT
+
+local message = {}
+message.code = 200
+
+local req_method = ngx.req.get_method()
+local args = ngx.req.get_uri_args()
+local from_date = tonumber(args.from) or (ngx.time() - 2*60*60)
+local size = tonumber(args.size) or (100)
+
+local must_array = {}
+if args.albumId then
+  table.insert(must_array,{match = { albumId = args.albumId }})
+end
+
+
+table.insert(must_array,{match = { media = 1 }})
+table.insert(must_array,{match = { source = 2 }})
+
+local body = {
+    size = size,
+    query = {
+        bool = {
+            must = must_array,
+        }
+    }
+}
+local count = 0
+while true do
+  local resp, status = meta_dao:search(body)
+  -- log(ERR,"searchUnDigest:" .. cjson_safe.encode(resp) .. ",status:" .. status)
+  if resp and resp.hits and resp.hits.hits then
+     local hits = resp.hits.hits
+     for _, mv in ipairs(hits) do
+        meta_dao:delete(mv._id)
+        ssdb_meta:remove(mv._id)
+        count = count + 1
+        log(ERR,"removeMeta:" .. mv._id .. ",count:" .. count)
+     end
+     if #hits < 1 then
+        break
+     end
+  else 
+    break
+  end
+end
+message.count = count
+local body = cjson_safe.encode(message)
+ngx.say(body)
