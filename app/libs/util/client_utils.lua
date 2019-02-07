da35a5f83606f0f2c82aeceaf6419a7fa23e6742
lilinchong <lilinchong@baidu.com>
2019-02-07 18:01:46 +0800

    change ssdb error log

--- app/libs/util/client_utils.lua
+++ app/libs/util/client_utils.lua
@@ -1,8 +1,4 @@
-local ok, new_tab = pcall(require, "table.new")
-if not ok or type(new_tab) ~= "function" then
-    new_tab = function (narr, nrec) return {} end
-end
-local _M = new_tab(0, 2)
+local _M = {}
 _M._VERSION = '0.01'
 
 
--- lua/ssdb/client.lua
+++ lua/ssdb/client.lua
@@ -2,7 +2,7 @@ local util_table = require "util.table"
 
 local ok, new_tab = pcall(require, "table.new")
 if not ok or type(new_tab) ~= "function" then
-    new_tab = function (narr, nrec) return {} end
+    new_tab = function(narr, nrec) return {} end
 end
 local _M = new_tab(0, 2)
 _M._VERSION = '0.01'
@@ -15,26 +15,26 @@ local ssdb = require "resty.ssdb"
 function _M.newClient()
     local ssdbClient = ssdb:new()
     ssdbClient:set_timeout(1000) -- 1 sec
-    local ok, err = ssdbClient:connect(context.SSDB_HOST, context.SSDB_PORT)
+    local _, err = ssdbClient:connect(context.SSDB_HOST, context.SSDB_PORT)
     if err then
-        ngx.say("failed to connect[" .. context.SSDB_HOST .. ":" .. context.SSDB_PORT .. "],cause:", err)
+        string.error("ssdbErr,connect[", context.SSDB_HOST .. ":" .. context.SSDB_PORT, "],cause:", err)
         return nil, err
     end
     return ssdbClient
 end
 
-function _M:open( )
-   return _M:newClient()
+function _M:open()
+    return _M:newClient()
 end
 
-function _M:close( client )
-   if util_table.isNull(client) then
-      return
-   end
-   local ok, err = client:set_keepalive(0, 20)
-   if err then
-      log(ERR,"failed to set keepalive:", err)
-   end
+function _M:close(client)
+    if util_table.isNull(client) then
+        return
+    end
+    local _, err = client:set_keepalive(0, 20)
+    if err then
+        string.error("ssdbErr,close[", context.SSDB_HOST .. ":" .. context.SSDB_PORT, "],cause:", err)
+    end
 end
 
 return _M
